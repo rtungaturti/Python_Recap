42c09cd16e1e2fd46e533fd2cadff03e13503ddb
rtungaturti <rtungaturti@gmail.com>
2025-04-09 05:18:23 +0530

    3rd commit -strings

---
+++ strings_title.py
@@ -0,0 +1,19 @@
+name="Raghu rama sastry"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("Hello\t" + name.upper())
+print("Hello\n\t" + name.upper())
+langs="                           		Python "
+print(langs)
+print("*********************************************************")
+langs=langs.rstrip()
+print(langs)
+langs=langs.lstrip()
+print(langs)
+apostrophe="Raghu's Fav Lang is Python"
+print(apostrophe)
+#apostrophe='Raghu's Fav Lang is Python' # Produces Error
+print(apostrophe)
+
+
